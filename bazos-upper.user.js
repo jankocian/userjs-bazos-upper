12dd48509d8a29468f30d842d9d15a0973614249
jankocian <mail@jankocian.com>
2022-03-18 22:23:02 +0100

    Removed comments

--- bazos-upper.user.js
+++ bazos-upper.user.js
@@ -8,21 +8,7 @@
 // @match        https://*.bazos.cz/moje-inzeraty.php
 // @match        https://*.bazos.cz/insert.php
 // @require      https://code.jquery.com/jquery-1.12.4.min.js
-
 // ==/UserScript==
-
-
-// ToDo:
-// - turn on upperAll based on GET variable (so that it can be bookmarked)
-// - add a random delay as a safety feature (multiplier to be configured at the top of the script)
-
-// Done:
-// - fix for one-image (and no-image) ads
-
-
-// v2:
-// - upload to sbazar
-// - sbazar upper (refactoring to reuse majority of the code)
 
 (function() {
     'use strict';
